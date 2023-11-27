65aca62a91087c6aa9e042292862942a9891c68d
adityasusawat <akablackdevils300@gmail.com>
2023-11-27 13:02:47 +0530

    modified UserSchema

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path"; //properly set the paths when we configure directories
 import { fileURLToPath } from "url";
-import { error } from "console";
+//import { error } from "console";
+//import { register } from "module";
+import { register } from "./controllers/auth.js";
 
 /* CONFIGURATION for middleware and different packages*/
 
@@ -20,32 +22,38 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true}));
-app.use*(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use * bodyParser.urlencoded({ limit: "30mb", extended: true });
 app.use(cors()); // will invoke our cross origin resource sharing policy
-app.use("/assets", express.static(path.join(__dirname, 'public/assets'))); //sets the directory of where we keep our assets
+app.use("/assets", express.static(path.join(__dirname, "public/assets"))); //sets the directory of where we keep our assets
 
 /* FILE STORAGE SETUP*/
 
-const storage = multer.diskStorage({ //this is how we save a file anytime someone uploads something
-    destination: function (req, file, cb) {
-        cb (null, "public/assets"); //will get saved in this folder
-    },
-    filename: function (req, file, cb) {
-        cb (null, file.originalname);
-    }
+const storage = multer.diskStorage({
+  //this is how we save a file anytime someone uploads something
+  destination: function (req, file, cb) {
+    cb(null, "public/assets"); //will get saved in this folder
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({ storage }); //anytime we need to upload a file, we will be using this variable
 
+/* ROUTES WITH FILES */
+
+app.post("/auth/register", upload.single("picture"), register); //locally uploads the picture in "public/assets"
+
 /* MONGOOSE SETUP */
 
 const PORT = process.env.PORT || 6001; //backup port
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlparse: true,
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => {
+  })
+  .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-})
-.catch((error) => console.log(`${error} did not connect`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
---
+++ models/User.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose"; //will help set up our model
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true, //can't have duplicate emails
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true } //gives us automatically dates when it is created and updated
+);
+
+const User = mongoose.model("User", UserSchema); //when we create a mongoose model, we create this schema first
+
+export default User;
