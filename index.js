246e1f323d5296536de915f9648025805a8888dd
adityasusawat <akablackdevils300@gmail.com>
2023-11-29 13:36:09 +0530

    backup

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATION for middleware and different packages*/
 
@@ -66,5 +69,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD THIS SAMPLE DATA ONLY ONCE */
+    //User.insertMany(users);
+    //Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
